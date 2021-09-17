28473faab767d866501df6f47e3950eae230d931
Rodney Matambo <rodney@gatsbyjs.com>
2021-09-16 22:02:53 -0400

    added code comments and fixed a bug

--- bin/index.js
+++ bin/index.js
@@ -2,36 +2,42 @@
 
 const execa = require("execa");
 
-const targetDir = process.cwd();
+// handle config options: Load all environment variables and arguments that are needed that are needed
+const version = `@${process.env.GATSBY_VERSION_TAG || "next"}`;
+const isfindingPackage = Boolean(process.env.PACKAGE_TYPE);
+const regexp = new RegExp(process.env.PACKAGE_TYPE || "alpha-9689ff", "i");
+const command = process.argv.slice(2).join(" ") || "npm i";
+const projectRoot = process.cwd();
+
+// find package.json file and its dependencies
 let deps;
 try {
-  deps = require(`${targetDir}/package.json`).dependencies;
+  deps = require(`${projectRoot}/package.json`).dependencies;
 } catch (error) {
   return console.log(
     "you must be in a directory that has a package.json file at the same level"
   );
 }
-const version = `@${process.env.GATSBY_VERSION_TAG || "next"}`;
-const isfindingPackage = Boolean(process.env.PACKAGE_TYPE);
-const regexp = new RegExp(process.env.PACKAGE_TYPE || "alpha-9689ff", "i");
-let command = process.argv.slice(2);
+
+// find the desired dependencies based on config
 const keys = Object.keys(deps);
 const desiredDeps = keys.filter((key) => {
-  let currentlyValue = key;
-  if (isfindingPackage) {
+  let currentValue = key;
+  if (!isfindingPackage) {
     currentValue = deps[key];
   }
-  return regexp.test(currentlyValue);
+  return regexp.test(currentValue);
 });
 
-command = command.join(" ") || "npm i";
-const output = `${command} ${desiredDeps.join(`${version} `)}${version}`;
-
-if (new RegExp(`${command} ${version}`).test(output))
+// test that the dependencies were found and create the cli command
+if (desiredDeps.length === 0)
   return console.log(
     "We found your package.json but it looks like there are no packages that match what you're looking for in this package.json."
   );
 
+const output = `${command} ${desiredDeps.join(`${version} `)}${version}`;
+
+// echo the command to the console
 (async () => {
   const { stdout } = await execa("echo", [output]);
   console.log(stdout);
