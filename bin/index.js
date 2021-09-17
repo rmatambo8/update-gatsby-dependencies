8732c731f5c6ebbcc7c3ce9c0bc1c2d2755031df
Rodney Matambo <rodney@gatsbyjs.com>
2021-09-16 20:29:12 -0400

    clean up scripts and remove install command

--- bin/index.js
+++ bin/index.js
@@ -18,6 +18,9 @@ const alphaDeps = keys.filter((key) => /alpha-9689ff/.test(deps[key]));
 command = command.join(" ") || "npm i";
 const output = `${command} ${alphaDeps.join("@next ")}@next`;
 
+if (/npm i @next/.test(output))
+  return console.log("must have a package to install");
+
 (async () => {
   const { stdout } = await execa("echo", [output]);
   console.log(stdout);
