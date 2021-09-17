cbdd531e9f0f46ada6b73010aa0198c9b2807b96
Rodney Matambo <rodney@gatsbyjs.com>
2021-09-16 20:53:52 -0400

    fixing import bug and adding package type support

--- bin/index.js
+++ bin/index.js
@@ -11,22 +11,25 @@ try {
     "you must be in a directory that has a package.json file at the same level"
   );
 }
-const isfindingPackage = Boolean(process.env.PACKAGE_TYPE)
-const regexp = new RegExp(process.env.PACKAGE_TYPE || "alpha-9689ff", "i")
+const isfindingPackage = Boolean(process.env.PACKAGE_TYPE);
+const regexp = new RegExp(process.env.PACKAGE_TYPE || "alpha-9689ff", "i");
 let command = process.argv.slice(2);
 const keys = Object.keys(deps);
 const desiredDeps = keys.filter((key) => {
-  let currentlyValue = key
+  let currentlyValue = key;
   if (isfindingPackage) {
-    currentValue = deps[key]
+    currentValue = deps[key];
   }
-  return regexp.test(currentlyValue));
-}
+  return regexp.test(currentlyValue);
+});
+
 command = command.join(" ") || "npm i";
 const output = `${command} ${desiredDeps.join("@next ")}@next`;
 
 if (/npm i @next/.test(output))
-  return console.log("We found your package.json but it looks like there are no packages that match what you're looking for in this package.json.");
+  return console.log(
+    "We found your package.json but it looks like there are no packages that match what you're looking for in this package.json."
+  );
 
 (async () => {
   const { stdout } = await execa("echo", [output]);
